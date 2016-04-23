db2c7ff6d194d56815d47791706994e830ae27eb
Vahid Heidari <crystal.delta@gmail.com>
2016-04-23 12:10:47 +0430

    Statistics script changed to document output format.

--- statistics/statistics.sh
+++ statistics/statistics.sh
@@ -20,7 +20,21 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+#
 # Web site: https://ask.wireshark.org/questions/13096/how-to-get-statistics-conversation-list-tcp-information-in-a-text-file
+#
+#
+# The out put of this script is not in csv (Comma Separated Values). If you want to get csv formated output,
+# you have to write your own script for converting the out put to csv, or use GUI form this address:
+#
+# https://ask.wireshark.org/questions/22749/how-to-export-the-pcap-file-statistics-with-csv-file-format
+#
+# If you need the data in CSV format, there are (at least) these options:
+#
+#    * Use the GUI: Statistics -> Conversations -> TCP -> Copy. This will copy the screen content as CSV into the clipboard.
+#    * Use tshark: tshark will not export the conversation data in CSV format, so you either convert it to CSV with Excel (while importing the data) or use a script (perl, python, watherver) to convert that output to csv.
+#    * Extend the tshark code to export CSV structured data.
+#
 
 if [[ $# < 1 ]]
 then
@@ -43,11 +57,12 @@ function stats()
 	tshark -r $1 -q -z conv,$2 > $3
 }
 
-stats $1  eth   eth.csv
-stats $1  fc    fc.csv
-stats $1  fddi  fddi.csv
-stats $1  ip    ip.csv
-stats $1  ipv6  ipv6.csv
-stats $1  ipx   ipx.csv
-stats $1  tcp   tcp.csv
-stats $1  udp   udp.csv
+stats $1  eth   eth.stat
+stats $1  fc    fc.stat
+stats $1  fddi  fddi.stat
+stats $1  ip    ip.stat
+stats $1  ipv6  ipv6.stat
+stats $1  ipx   ipx.stat
+stats $1  tcp   tcp.stat
+stats $1  udp   udp.stat
+
